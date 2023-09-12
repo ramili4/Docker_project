8beebb0321057a8d9d01eea6dbc77b0b2981234a
root <root@Omega.ramili4.com>
2023-09-12 11:56:21 +0500

    Docker_course_refresher

---
+++ python-app/main.py
@@ -0,0 +1,11 @@
+n = int(input ("Please enter a number: "))
+
+if n < 0:
+	print("A number must be > 0")
+else:
+	sum = 0
+	i = n 
+	while i > 0:
+		sum += i 
+		i -= 1
+	print("that's the code, motherfucker")
